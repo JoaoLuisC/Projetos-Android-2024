4fdb04c0455c886141031f9a2fef3098f57f21b5
João Luis Carodoso <joao.lc12@hotmail.com>
2024-07-18 10:22:56 -0300

    first commit

---
+++ AndroidStudioProjects/Netflix/app/src/main/java/br/edu/ifsuldeminas/mch/netflix/Trabalho04Activity.java
@@ -0,0 +1,13 @@
+package br.edu.ifsuldeminas.mch.netflix;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Trabalho04Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trabalho04);
+    }
+}
