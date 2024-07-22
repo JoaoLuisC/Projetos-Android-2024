550d2d07ce156a14cf70c8d7cbc15f4fd5ff774c
João Luis Carodoso <joao.lc12@hotmail.com>
2024-07-22 10:44:52 -0300

    att Project Netflix

--- AndroidStudioProjects/Netflix/app/src/main/java/br/edu/ifsuldeminas/mch/netflix/MainActivity.java
+++ AndroidStudioProjects/Netflix/app/src/main/java/br/edu/ifsuldeminas/mch/netflix/MainActivity.java
@@ -5,51 +5,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private VideoPlayer videoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.home_page);
 
-        VideoView videoView = findViewById(R.id.mainVideo);
-        SeekBar seekBar = findViewById(R.id.currentViewBar);
-        TextView timeElapsed = findViewById(R.id.timeElapsed);
-        TextView timeTotal = findViewById(R.id.timeTotal);
-        ImageButton playPauseButton = findViewById(R.id.buttomPlay);
-
-        videoPlayer = new VideoPlayer(this, videoView, seekBar, timeElapsed, timeTotal, playPauseButton);
-
-        Button btnTrabalho2 = findViewById(R.id.btnTrabalho2);
-        Button btnTrabalho4 = findViewById(R.id.btnTrabalho4);
-
-        btnTrabalho2.setOnClickListener(new View.OnClickListener() {
+        Button btnTrabalhoNetflix = findViewById(R.id.btnTrabalhoNetflix);
+        btnTrabalhoNetflix.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Trabalho02Activity.class);
+                Intent intent = new Intent(MainActivity.this, NetflixActivity.class);
                 startActivity(intent);
             }
         });
 
-        btnTrabalho4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Trabalho04Activity.class);
-                startActivity(intent);
-            }
-        });
+
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        videoPlayer.release();
-    }
+
 }
